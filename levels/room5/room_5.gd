d7e349d676bf3970a7d8d54fd34c016354b64c58
entityJY <jomathya07@gmail.com>
2026-02-01 16:10:16 -0800

    remove signal for returning to menu

--- levels/room5/room_5.gd
+++ levels/room5/room_5.gd
@@ -4,7 +4,6 @@ func credits():
 	print("blah")
 	var tween = get_tree().create_tween()
 	tween.tween_property(GlobalFlags.camera, "position", Vector2(GlobalFlags.camera.position.x, GlobalFlags.camera.position.y-960), 1)
-	GlobalFlags.return_to_menu.emit()
 
 
 func _on_room_complete() -> void:
--- scripts/global_flags.gd
+++ scripts/global_flags.gd
@@ -12,7 +12,6 @@ signal key_obtained()
 signal go_to_final_level()
 signal cat_awakened()
 signal enter_room(room_id: int)
-signal return_to_menu
 
 func _ready():
     room1_data = Room1Data.new()
