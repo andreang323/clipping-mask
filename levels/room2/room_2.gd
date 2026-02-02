ffaa0d8187b1a6ff8823ab36025c1f9a42ff5421
entityJY <jomathya07@gmail.com>
2026-02-01 16:26:26 -0800

    Fix cat animation issue

--- levels/room2/room_2.gd
+++ levels/room2/room_2.gd
@@ -14,7 +14,8 @@ func _ready() -> void:
 func _on_correct_color_received(_id) -> void:
 	for receiver in needed_receivers:
 		if receiver.correct == false: return
-	awaken_cat()
+	if !GlobalFlags.room2_data.cat_awakened:
+		awaken_cat()
 	for receiver in other_receivers:
 		if receiver.correct == false: return
 	GlobalFlags.room2_data.room_complete = true
